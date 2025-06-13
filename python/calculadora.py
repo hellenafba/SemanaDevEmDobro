99ab2581899b403974d854c4cca8899b6bc85854
Helena <112509813+hellenafba@users.noreply.github.com>
2025-06-13 19:54:45 -0300

    Add files via upload

---
+++ python/calculadora.py
@@ -0,0 +1,17 @@
+x = int(input("insira um número: "))
+y = int(input("insira outro número:"))
+
+z = (x+y)/2
+
+print(f"({x} + {y})/2 = {z}")
+
+soma_total = x + y + z
+
+print(f"a soma da média, do y e do x é: {soma_total}")
+
+list = [x, y, z]
+
+maior = list.max
+
+print(maior)
+
